540dd4c706f4c16fe57337891a3a8d05ad0e1d29
Kalle Kurki <kallekurki@kamk.fi>
2022-04-02 16:37:53 +0300

    Add player movement and KUOLEMA

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class PlayerMovement : MonoBehaviour
 {
     public float playerSpeed = 5.0f;
@@ -10,6 +10,7 @@ public class PlayerMovement : MonoBehaviour
     Rigidbody2D rb;
     BoxCollider2D playerCollider;
 
+    public float CameraFollowSpeed = 2.0f;
     private bool IsGrounded()
     {
         RaycastHit2D hit = Physics2D.Raycast(new Vector2(transform.position.x - transform.localScale.x / 2, transform.position.y - playerHeight / 2 - 0.1f),
@@ -23,9 +24,59 @@ public class PlayerMovement : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         playerCollider = GetComponent<BoxCollider2D>();
     }
-    
+
+    void UpdateCameraPosition()
+    {
+        if (transform.position.y > Camera.main.transform.position.y)
+        {
+            Camera.main.transform.position = new Vector3(Camera.main.transform.position.x,
+                Camera.main.transform.position.y + Time.deltaTime * CameraFollowSpeed,
+                Camera.main.transform.position.z);
+        }
+    }
+
+    void StopTheGame()
+    {
+        //TODO move to main menu
+        Scene scene = SceneManager.GetActiveScene(); SceneManager.LoadScene(scene.name);
+    }
+
+    void CheckThatPlayerIsAlive()
+    {
+        //Check that players y > Lowest edge of camera.
+        if (transform.position.y - Camera.main.ScreenToWorldPoint(new Vector2(0, 0)).y < 0)
+        {
+            StopTheGame();
+        }
+
+        float colliderWidth = playerCollider.size.x;
+
+        //Check that there isn't block above the player
+        RaycastHit2D hit = Physics2D.Raycast(new Vector2(transform.position.x - colliderWidth / 2, transform.position.y + playerHeight / 2 + 0.1f),
+           Vector2.right,
+           colliderWidth);
+
+        if (null != hit.transform)
+        {
+            Rigidbody2D colliderRb = hit.transform.GetComponent<Rigidbody2D>();
+            if (null != colliderRb && 5 < Mathf.Abs(colliderRb.velocity.y))
+            {
+                //Stop the game if object have velocity
+                StopTheGame();
+            }
+        }
+
+        Debug.DrawRay(new Vector2(transform.position.x - colliderWidth / 2, transform.position.y + playerHeight / 2 + 0.2f), Vector2.right * colliderWidth, Color.red);
+    }
+
+
     void Update()
     {
+
+        UpdateCameraPosition();
+
+        CheckThatPlayerIsAlive();
+
         Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
      
