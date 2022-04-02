9b722409cb267f840f21e4624b9ce31adfe84277
eemelik <eemelikalliokoski@kamk.fi>
2022-04-02 16:23:48 +0300

    Add missing changes

--- Assets/Scripts/FallingObject.cs
+++ Assets/Scripts/FallingObject.cs
@@ -14,42 +14,14 @@ public class FallingObject : MonoBehaviour
     void Start()
     {
         bc = GetComponent<BoxCollider2D>();
-
         rb = GetComponent<Rigidbody2D>();
-        //rb.AddForce(transform.up * -100f);
-        //rb.velocity = new Vector2(0, -50);
     }
 
-    // Update is called once per frame
-    // void Update()
-    // {
-    //     if (freeFall) {
-    //         
-    //     }
-    // }
     void FixedUpdate()
     {
-        // RaycastHit2D hit = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y - transform.localScale.y / 2),
-        //    Vector2.down,
-        //    .1f);
-        //
-        // Debug.DrawRay(new Vector2(transform.position.x, transform.position.y - transform.localScale.y/2), Vector2.down * 0.1f, Color.red);
-        //
-        // if (hit.transform == null) {
         if (rb.velocity.magnitude > maxSpeed && freeFall) {
             rb.velocity = rb.velocity.normalized * maxSpeed;
         }
-       
-        //transform.Translate(0, -2f * Time.deltaTime, 0);
-
-        // var currentVelocity = rb.velocity;
-        //
-        // if (currentVelocity.y <= 0f)
-        //     return;z§
-        //
-        // currentVelocity.y = 0f;
-        //
-        // rb.velocity = currentVelocity;
     }
 
     void OnCollisionEnter2D(Collision2D collision)
@@ -64,15 +36,5 @@ public class FallingObject : MonoBehaviour
                 rb.mass = 1999;
             }
         }
-
     }
-    
-    //void OnCollisionExit2D(Collision2D collision)
-    //{
-    //    collisionCount--;
-    //    if (collisionCount == 0) {
-    //        Debug.Log("aaaa");
-    //        freeFall = true;
-    //    }
-    //}
 }
