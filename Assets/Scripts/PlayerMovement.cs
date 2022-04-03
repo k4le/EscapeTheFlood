5e2fbfb986e51d18b53841ea719aa71871cfb47a
Kalle Kurki <kallekurki@kamk.fi>
2022-04-03 11:28:03 +0300

    uncomment AddScore()

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -135,7 +135,7 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        //AddScore();
+        AddScore();
 
         UpdateCameraPosition();
         CheckThatPlayerIsAlive();
